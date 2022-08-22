aef9d7533b2af90d89c27cda78a1774f715efc9e
Ulug'bek <ulugbekgozixonov@gmail.com>
2022-08-22 13:45:12 +0500

    Second Commit

--- src/main/java/arfmetika/publishprojectwithgit/entity/Talaba.java
+++ src/main/java/arfmetika/publishprojectwithgit/entity/Talaba.java
@@ -1,10 +1,15 @@
 package arfmetika.publishprojectwithgit.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Talaba {
 
     @Id
