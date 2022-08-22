9a691882a29d3199fa7cdc60574769fa89ba3eca
Ulug'bek <ulugbekgozixonov@gmail.com>
2022-08-22 13:58:57 +0500

    Second Commit

---
+++ src/main/java/arfmetika/publishprojectwithgit/repo/TalabaRepository.java
@@ -0,0 +1,7 @@
+package arfmetika.publishprojectwithgit.repo;
+
+import arfmetika.publishprojectwithgit.entity.Talaba;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TalabaRepository extends JpaRepository<Talaba,Integer> {
+}
